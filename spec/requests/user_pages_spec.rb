c0c859daa3474ce2f907fb82e52171f6f7c9c60b
Greg Daviet <Greg@theDaviets.com>
2012-08-23 08:29:49 -0600

    Change Gravatr 'change' link to open in new browser tab

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -128,6 +128,7 @@ describe "User" do
 			it { should have_selector "title",	text: "Edit user" }
 			it { should have_selector "h1",			text: "Update your profile" }
 			it { should have_link			"change",	href: "http://gravatar.com/emails" }
+			specify { find_link('change')[:target].should == '_blank'}
 			it { should have_selector "img",		src: "gravatar.com/avatar/" }
 		end
 
