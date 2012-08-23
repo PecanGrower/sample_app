bd805ffdf01b24396173ca1b0bdd15c7a30fcafd
Greg Daviet <Greg@theDaviets.com>
2012-08-23 08:30:57 -0600

    Add test to ensure 'Profile' and 'Settings' links are not available when signed out

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -34,11 +34,13 @@ describe "Authentication" do
 			it { should have_link 'Profile', 	href: user_path(user) }
 			it { should have_link 'Settings', href: edit_user_path(user) }
 			it { should have_link 'Sign out', href: signout_path }
-			it { should_not have_link 'Sign in', href: signin_path }
+			it { should_not have_link 'Sign in' }
 
 			describe "followed by signout" do
 				before { click_link "Sign out" }
 				it { should have_link 'Sign in', href: signin_path }
+				it { should_not have_link 'Profile' }
+				it { should_not have_link 'Settings' }
 			end
 		end
 	end
