dcd68e7c21a88284e9b9f5626fdf1cbcbc68019b
Greg Daviet <Greg@theDaviets.com>
2012-08-25 22:40:54 -0600

    Add follower stats partial to user home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 SampleApp::Application.routes.draw do
-  resources :users
+  resources :users do
+    member do
+      get 'following'
+      get 'followers'
+    end
+  end
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
 
